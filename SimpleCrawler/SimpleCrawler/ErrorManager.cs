b74c932515e46a0ab48420f975352942afa81376
Krzysztof <roidos@outlook.com>
2019-03-06 01:36:24 +0100

    added simple crawler

---
+++ SimpleCrawler/SimpleCrawler/ErrorManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleCrawler
+{
+    public sealed class ErrorManager
+    {
+        private TextBlock _errorBox;
+        private static readonly Lazy<ErrorManager> _lazyInstance = new Lazy<ErrorManager>(() => new ErrorManager());
+
+        public static ErrorManager Instance
+        {
+            get
+            {
+                return _lazyInstance.Value;
+            }
+        }
+        
+        ErrorManager()
+        {
+            MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+            _errorBox = mainWindow.ErrorBox;
+        }
+
+        public void SetErrorText(string text)
+        {
+            _errorBox.Text = text;
+        }
+    }
+}
